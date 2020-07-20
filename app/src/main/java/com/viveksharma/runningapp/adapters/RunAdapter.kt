ef6edf2692d440e52a7a6f0d1ee59e1cc111b69b
viveksharma2382000 <viveksharma2382000@gmail.com>
2020-07-20 18:26:09 +0530

    Showing Runs and sorting them

---
+++ app/src/main/java/com/viveksharma/runningapp/adapters/RunAdapter.kt
@@ -0,0 +1,88 @@
+package com.viveksharma.runningapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.viveksharma.runningapp.R
+import com.viveksharma.runningapp.db.Run
+import com.viveksharma.runningapp.other.TrackingUtility
+import kotlinx.android.synthetic.main.item_run.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class RunAdapter : RecyclerView.Adapter<RunViewHolder>() {
+
+    private var differCallback = object : DiffUtil.ItemCallback<Run>() {
+        override fun areItemsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, differCallback)
+
+    fun submitList(list: List<Run>) = differ.submitList(list)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        return RunViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_run, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+        val currentRun = differ.currentList[position]
+        holder.bind(currentRun)
+    }
+}
+
+class RunViewHolder(
+    itemView: View
+) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(run: Run) {
+        itemView.apply {
+            Glide.with(this).load(run.img).into(ivRunImage)
+
+            val calendar = Calendar.getInstance().apply {
+                timeInMillis = run.timestamp
+            }
+            val dateFormat = SimpleDateFormat("dd.MM.yy", Locale.getDefault())
+            tvDate.text = dateFormat.format(calendar.time)
+
+            val avgSpeed = "${run.avgSpeedInKMH}km/h"
+            tvAvgSpeed.text = avgSpeed
+
+            val distanceInKm = "${run.distanceInMeters / 1000f}km"
+            tvDistance.text = distanceInKm
+
+            tvTime.text = TrackingUtility.getFormattedStopWatchTime(run.timeInMillis)
+
+            val caloriesBurned = "${run.caloriesBurned}kcal"
+            tvCalories.text = caloriesBurned
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
